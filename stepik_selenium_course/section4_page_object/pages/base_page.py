e7041f8e0daef8e9e5179d3f883ee0c80bea92a6
Eldar Adzhiev <e.adzhiev93@gmail.com>
2022-07-18 10:15:16 +0300

    Проверка элемента на странице

--- stepik_selenium_course/section4_page_object/pages/base_page.py
+++ stepik_selenium_course/section4_page_object/pages/base_page.py
@@ -1,8 +1,17 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage:
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(10)
 
     def open(self):
         self.browser.get(self.url)
-    
+
+    def is_element_present(self, how_search, what_search):
+        try:
+            self.browser.find_element(how_search, what_search)
+        except NoSuchElementException:
+            return False
+        return True
