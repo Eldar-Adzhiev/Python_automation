ef1b5a42fea9327c38f814f9d560378600a307b3
Eldar Adzhiev <e.adzhiev93@gmail.com>
2022-10-16 16:17:13 +0300

    Добавил BasePageLocators

--- stepik_selenium_course/section4_page_object/pages/locators.py
+++ stepik_selenium_course/section4_page_object/pages/locators.py
@@ -1,12 +1,8 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
 class ProductPageLocators():
@@ -16,4 +12,10 @@ class ProductPageLocators():
     BOOK_NAME_BASKET = (By.CSS_SELECTOR, ".alert.alert-safe.alert-success.fade.in .alertinner strong")
     BOOK_PRICE_BASKET = (By.CSS_SELECTOR, ".alertinner p strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div:nth-child(1)")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group a")
+
 
