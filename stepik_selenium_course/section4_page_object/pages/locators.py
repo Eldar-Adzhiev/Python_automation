36aaa601fde460f109f31d6362b0964489f41401
Eldar Adzhiev <e.adzhiev93@gmail.com>
2022-10-16 11:25:28 +0300

    Добавил локаторы для страницы Product

--- stepik_selenium_course/section4_page_object/pages/locators.py
+++ stepik_selenium_course/section4_page_object/pages/locators.py
@@ -9,4 +9,11 @@ class LoginPageLocators:
 
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    BOOK_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    BOOK_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    BOOK_NAME_BASKET = (By.CSS_SELECTOR, ".alert.alert-safe.alert-success.fade.in .alertinner strong")
+    BOOK_PRICE_BASKET = (By.CSS_SELECTOR, ".alertinner p strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div:nth-child(1)")
 
