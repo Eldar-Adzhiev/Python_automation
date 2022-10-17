de76b81b0cf16ebaae2be4d5e472804eef272335
Eldar Adzhiev <e.adzhiev93@gmail.com>
2022-10-17 07:57:59 +0300

    Добавил BasketPageLocators

--- stepik_selenium_course/section4_page_object/pages/locators.py
+++ stepik_selenium_course/section4_page_object/pages/locators.py
@@ -5,6 +5,7 @@ class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
+
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
     BOOK_NAME = (By.CSS_SELECTOR, '.product_main h1')
@@ -13,9 +14,12 @@ class ProductPageLocators():
     BOOK_PRICE_BASKET = (By.CSS_SELECTOR, ".alertinner p strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div:nth-child(1)")
 
+
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LINK = (By.CSS_SELECTOR, ".btn-group a")
 
 
+class BasketPageLocators():
+    EMPTY_BASKET = (By.XPATH, "//p[contains(text(),'basket is empty')]")
