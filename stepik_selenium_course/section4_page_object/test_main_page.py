b2896831a458337ff2513349c71dcbb26b539132
Eldar Adzhiev <e.adzhiev93@gmail.com>
2022-10-17 07:58:16 +0300

    Добавил тест test_guest_cant_see_product_in_basket_opened_from_main_page

--- stepik_selenium_course/section4_page_object/test_main_page.py
+++ stepik_selenium_course/section4_page_object/test_main_page.py
@@ -1,5 +1,9 @@
+import pytest
+
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
+
 
 def test_guest_can_go_to_login_page(browser):
     """
@@ -11,11 +15,13 @@ def test_guest_can_go_to_login_page(browser):
     login_page = page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     login_page.should_be_login_page()
 
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
 
 def test_guest_should_see_login_page(browser):
     """
@@ -25,3 +31,11 @@ def test_guest_should_see_login_page(browser):
     login_page = LoginPage(browser, link)
     login_page.open()
     login_page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_empty_basket()
