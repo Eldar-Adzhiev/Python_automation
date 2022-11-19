b07570a5897712856b3ef0b9937bda6f394954e6
Eldar Adzhiev <e.adzhiev93@gmail.com>
2022-11-19 13:15:46 +0300

    main fail

---
+++ LearnQA_Python_API/main.py
@@ -0,0 +1,9 @@
+import requests
+data = {
+        'email': 'vinkotov@example.com',
+        'password': '1234'
+        }
+response1 = requests.post("https://playground.learnqa.ru/api/user/login",
+                                  data=data)
+
+print(response1.cookies)
