2a136b616f2de1b2a19c8df70113aa8299a721e2
Eldar Adzhiev <e.adzhiev93@gmail.com>
2022-12-17 10:48:55 +0300

    Реализован метод для извлечения контактов из БД

--- barancev_python_training_course/fixture/db.py
+++ barancev_python_training_course/fixture/db.py
@@ -1,4 +1,6 @@
 import mysql.connector
+
+from barancev_python_training_course.model.contact import Contact
 from barancev_python_training_course.model.group import Group
 
 
@@ -20,6 +22,18 @@ class DbFixture:
             for row in cursor:
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
         finally:
             cursor.close()
         return list
